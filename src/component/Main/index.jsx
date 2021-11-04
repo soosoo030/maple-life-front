a438097009c70d94583159fc56e712e1b25ca61e
soosoo030 <whereru511@gmail.com>
2021-11-04 19:56:26 +0900

    ui: 로그인 안내 css 추가

--- src/component/Main/index.jsx
+++ src/component/Main/index.jsx
@@ -1,5 +1,6 @@
 import Login from '../Login';
 import { useSetRecoilState } from 'recoil';
+import { Grid, Button, Typography } from '@mui/material';
 import openPageAtom from '../../atoms/page';
 
 export default function Main() {
@@ -8,7 +9,52 @@ export default function Main() {
   return (
     <div>
       <Login />
-      <button onClick={() => setLoginOpen(true)}>login</button>
-    </div>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="stretch"
+        direction="column"
+        flexWrap="nowrap"
+        sx={{
+          width: '580px',
+          height: '404px',
+          background: '#FFFFFF',
+          borderRadius: '55px',
+          textAlign: 'center'
+        }}
+      >
+        <Grid item>
+          <Typography
+            variant="h1"
+            fontFamily="'Slabo 27px'"
+            align="center"
+            color="secondary.main"
+            pt={5}
+            mb={5}
+          >
+            MAPLE LIFE
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography
+            variant="h6"
+            align="center"
+            mb={5}
+          >
+            로그인을 하면 MAPLE LIFE의 서비스를 이용하실 수 있습니다
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            onClick={() => setLoginOpen(true)}
+            sx={{
+              color: 'background.default',
+              backgroundColor: 'primary.main',
+              justifyContent: 'center',
+            }}
+          >로그인 페이지로 이동</Button>
+        </Grid>
+      </Grid>
+    </div >
   );
 }
